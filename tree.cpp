87a4811e514430080e6d6a6dc57c85fde715fc94
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-09-02 21:29:02 +0430

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,14 +1,1 @@
-#ifndef _IOSTREAM_H
-#include <iostream.h>\rder(TreeNode *CurrentNode)
-{\
-Tree::Tree(const Tree& s)
-  else return 0;
-}
-
-// Driver
-int operator==(const Tree& s, const Tree& t)ame
-cout << endl;
-if (s == t) cout << 1; else cout << 0;
-cout << endl;
-}
 
