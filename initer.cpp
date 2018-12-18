aa8918687090224e99d51018b61291150edad2cc
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-12-18 21:35:00 +0330

    Update initer.cpp

--- initer.cpp
+++ initer.cpp
@@ -1,6 +1,1 @@
-#ifndef _IOSTREAM_H
-#include <iostream.h>
-#define _IOSTREAMtNode = *s.Delete(CurrentNode);
-	    char& temp = CurrentNode->data;
-	    CurrentNode = CurrentNode->RightChild;
-	    return &t
+
