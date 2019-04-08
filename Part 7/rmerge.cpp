efa72f64d98fa25b2529da94e06f6e8756aa9017
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-04-08 20:07:47 +0430

    Update rmerge.cpp

--- Part 7/rmerge.cpp
+++ Part 7/rmerge.cpp
@@ -1,27 +1,4 @@
-#include <iostream.h>
 
-
-class Element
-{
-public:
-   int key;
-   int other;
-   int link;
-   Element(){other = 0; link = 0;};
-};
-
-
-int ListMerge(Element *list, const int start1, const int start2)
-// The sorted linked lists whose first elements are indexed by @start@1 and @start@2,
-// respectively, are merged to obtain the sorted linked list. The index of the first element of the
-// sorted list is returned. Integer links are used.
-{
-   int iResult = 0;
-   for (int i1 = start1, i2 = start2; i1 && i2; )
-       if (list[i1].key <= list[i2].key) {
-	   list[iResult].link = i1;
-	   iResult = i1; i1 = list[i1].link;
-       }
        else {
 	   list[iResult].link = i2;
 	   iResult = i2; i2 = list[i2].link;
@@ -33,13 +10,6 @@ int ListMerge(Element *list, const int start1, const int start2)
 }
 
 
-int rMergeSort(Element *list, const int left, const int right)
-// List @list~=~(list[left],~... ,~list[right])@ is to be sorted on the field @key@.
-// @link@ is a link field in each record that is initially 0.
-// @rMergeSort@ returns the index of the first element in the sorted chain.
-// @list[0]@ is a record for intermediate results used only in @ListMerge@.
-{
-  if (left >= right) return left;
   int mid = (left + right)/2;
   return ListMerge( list, rMergeSort(list, left, mid), rMergeSort(list, mid+1, right));
 }
